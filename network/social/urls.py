901e25dfa50ce34da840a03e1dcf937db341ef32
andrii <andriifedotov@ukr.net>
2023-02-07 13:08:56 +0200

    Add User retrieve and update view

--- network/social/urls.py
+++ network/social/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import RegistrationAPIView, LoginAPIView
+from .views import RegistrationAPIView, LoginAPIView, UserRetrieveUpdateAPIView
 
 
 urlpatterns = [
     path('users/signup/', RegistrationAPIView.as_view()),
     path('users/login/', LoginAPIView.as_view()),
+    path('user', UserRetrieveUpdateAPIView.as_view()),
 ]
