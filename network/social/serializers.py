97a9654219b62f1fdfc4e7d55b99d1548b59139d
andrii <andriifedotov@ukr.net>
2023-02-07 13:08:27 +0200

    Add user serializer

--- network/social/serializers.py
+++ network/social/serializers.py
@@ -56,9 +56,37 @@ class LoginSerializer(serializers.Serializer):
 
         user.last_login = timezone.now()
         user.save()
-        
+
         return {
             'email': user.email,
             'username': user.username,
             'token': user.token
         }
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(
+        max_length=128,
+        min_length=8,
+        write_only=True
+    )
+
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'password', 'token',)
+        read_only_fields = ('token',)
+
+    def update(self, instance, validated_data):
+
+        password = validated_data.pop('password', None)
+
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+
+        if password is not None:
+            instance.set_password(password)
+
+        instance.save()
+
+        return instance
